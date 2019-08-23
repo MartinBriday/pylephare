50554d7b2d63bb214028cf093ba735e190f7c945
Martin Briday <m.briday@ipnl.in2p3.fr>
2019-08-23 10:02:22 +0200

    lephare code in progress...

--- sedkcorr/sed_fitting/lephare.py
+++ sedkcorr/sed_fitting/lephare.py
@@ -27,8 +27,86 @@ class LePhareSEDFitter( BaseObject ):
                    "GAL_FSCALE":1.,
                    "GAL_LIB":"LIB_BC03",
                    "SEL_AGE":"$LEPHAREDIR/sed/GAL/BC03_CHAB/BC03_AGE.list",
-                   "AGE_RANGE":(0.,13.55e9)
-                   "end":None}
+                   "AGE_RANGE":(0.,13.55e9),
+                   "FILTER_LIST":["galex/FUV.pb","galex/NUV.pb","sdss/up.pb","sdss/gp.pb","sdss/rp.pb","sdss/ip.pb","sdss/zp.pb"],
+                   "TRANS_TYPE":0,
+                   "FILTER_CALIB":0,
+                   "FILTER_FILE":"snf_galex_sdss.filt",
+                   "STAR_LIB_IN":"snf_LIB_STAR",
+                   "STAR_LIB_OUT":"snf_STAR",
+                   "QSO_LIB_IN":"snf_LIB_QSO",
+                   "QSO_LIB_OUT":"snf_QSO",
+                   "GAL_LIB_IN":"snf_LIB_BC03",
+                   "GAL_LIB_OUT":"snf_BC03",
+                   "MAGTYPE":"AB",
+                   "Z_STEP":(0.004,0.20,0.1),
+                   "COSMOLOGY":(70,0.3,0.7),
+                   "MOD_EXTINC":(1,27),
+                   "EXTINC_LAW":"LMC_Fitzpatrick.dat",
+                   "EB_V":(0.0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.6,0.7,0.8),
+                   "EM_LINES":"YES",
+                   #"Z_FORM":(8,7,6,5,4,3),
+                   "LIB_ASCII":"YES",
+                   "CAT_IN":"$LEPHAREDIR/data/SNf/SNf_mag_1kpc_124_lephare.csv",
+                   "INP_TYPE":"M",
+                   "CAT_MAG":"AB",
+                   "CAT_FMT":"MEME",
+                   "CAT_LINES":(1,100000),
+                   "CAT_TYPE":"LONG",
+                   "CAT_OUT":"$LEPHAREDIR/data/SNF/SNf_mag_1kpc_124_lephare.out",
+                   "PARA_OUT":"$LEPHAREDIR/config/snf_host_zphot_output.para",
+                   "BD_SCALE":0,
+                   "GLB_CONTEXT":-1,
+                   #"FORB_CONTEXT":-1,
+                   "ERR_SCALE":(0.052,0.026,0.05,0.02,0.02,0.02,0.03),
+                   "ERR_FACTOR":1.,
+                   "ZPHOTLIB":("snf_BC03","snf_STAR","snf_QSO"),
+                   "ADD_EMLINES":"YES",
+                   "FIR_LIB":"NONE",
+                   "FIR_LMIN":7.,
+                   "FIR_CONT":-1,
+                   "FIR_SCALE":-1,
+                   "FIR_FREESCALE":"NO",
+                   "FIR_SUBSTELLAR":"NO",
+                   "PHYS_LIB":"NONE",
+                   "PHYS_CONT":-1,
+                   "PHYS_SCALE":-1,
+                   "PHYS_NMAX":100000,
+                   "MASS_SCALE":(6.,16.),
+                   "MAG_ABS":(-10.,-26.),
+                   "MAG_REF":6,
+                   #"ZFORM_MIN":(5,5,5,5,5,5,3,1),
+                   "Z_RANGE":(0,0.20),
+                   "EBV_RANGE":(0.,1.),
+                   #"NZ_PRIOR":(4,2,4),
+                   "ZFIX":"YES",
+                   "Z_INTERP":"NO",
+                   "DZ_WIN":0.25,
+                   "MIN_THRES":0.1,
+                   #"PROB_INTZ":(0,0.5,0.5,1.,1.,1.5),
+                   "MABS_METHOD":1,
+                   "MABS_CONTEXT":-1,
+                   "MABS_REF":4,
+                   "MABS_FILT":(3,4,5,6),
+                   "MABS_ZBIN":(0,0.5,1,1.5,2,3,3.5,4),
+                   "SPEC_OUT":"YES",
+                   "CHI2_OUT":"NO",
+                   "PDZ_OUT":"NONE",
+                   #"PDZ_MABS_FILT":(2,10,14),
+                   "FAST_MODE":"NO",
+                   "COL_NUM":3,
+                   "COL_SIGMA":3,
+                   "COL_SEL":"AND",
+                   #"APPLY_SYSSHIFT":(0.007,-0.001,0.001,-0.003,0.006),
+                   "AUTO_ADAPT":"NO",
+                   "ADAPT_BAND":(4,2,4),
+                   "ADAPT_LIM":(10,22.0),
+                   "ADAPT_POLY":1,
+                   "ADAPT_METH":1,
+                   "ADAPT_CONTEXT":-1,
+                   "ADAPT_ZBIN":(0.01,0.1),
+                   "ADAPT_MODBIN":(1,1000),
+                   "ERROR_ADAPT":"NO"}
     
     PATH_LEPHAREDIR = os.path.expanduser(os.getenv("LEPHAREDIR"))
 
@@ -53,8 +131,8 @@ class LePhareSEDFitter( BaseObject ):
         for key, value in which.items():
             path = (pkg_resources.resource_filename(__name__, key+"/") + "/") if v is None else value
             self._side_properties["path_"+key] = path
-    
-    def _write_param_(self, line, new_param_value):
+
+    def _get_idx_line_(self, line):
         """
         
         """
@@ -69,11 +147,28 @@ class LePhareSEDFitter( BaseObject ):
                     break
         else:
             raise ValueError("'line' must be either integer (line index) or string (first word of the line)")
+        return idx_line
+    
+    def _get_new_param_value_(self, new_param_value):
+        """
         
-        if type(new_param_value) in [int, float]:
+        """
+        if new_param_value in ["YES", "yes", "Yes", True]:
+            new_param_value = "YES"
+        elif new_param_value in ["NO", "no", "No", False]:
+            new_param_value = "NO"
+        elif type(new_param_value) in [int, float]:
             new_param_value = str(new_param_value)
         elif type(new_param_value) in [tuple, list]:
-            new_param_value = ",".join(new_param_value)
+            new_param_value = ",".join([str(elt) for elt in new_param_value])
+        return new_param_value
+    
+    def _write_param_(self, line, new_param_value):
+        """
+        
+        """
+        idx_line = self._get_idx_line_(line)
+        new_param_value = self._get_new_param_value_(new_param_value)
         
         line_splited = file_buf[idx_line].split()
         line_splited[1] = new_param_value
